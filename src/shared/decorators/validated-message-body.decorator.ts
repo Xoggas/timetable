8c221a8945c4c5595d5d530ff637c9fb97b877a0
Xoggas <sashakor16@gmail.com>
2024-11-02 14:02:26 +0300

    Removed LessonTable classes

--- src/shared/decorators/validated-message-body.decorator.ts
+++
@@ -1,8 +0,0 @@
-import { PipeTransform, Type, ValidationPipe } from '@nestjs/common';
-import { MessageBody } from '@nestjs/websockets';
-
-export function ValidatedMessageBody(
-  ...pipes: (Type<PipeTransform> | PipeTransform)[]
-): ParameterDecorator {
-  return MessageBody(...pipes, new ValidationPipe());
-}
