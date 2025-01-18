034fc26fe69da3b4a719ad40da3cd563faa90c15
Xoggas <sashakor16@gmail.com>
2025-01-18 13:01:27 +0300

    Wrote integration tests for LessonController

--- TimeTable.Api.Tests.Integration/Lesson/LessonControllerTests.cs
+++ TimeTable.Api.Tests.Integration/Lesson/LessonControllerTests.cs
@@ -16,7 +16,7 @@ public sealed class LessonControllerTests : IClassFixture<MongoDbFixture>
     }
 
     [Fact]
-    public async Task Get_ShouldReturnEmptyCollection()
+    public async Task Get_ShouldReturnCollectionOfOneLesson()
     {
         var response = await _client.GetAsync("api/lesson");
 
@@ -53,9 +53,127 @@ public sealed class LessonControllerTests : IClassFixture<MongoDbFixture>
         var response = await _client.PostAsJsonAsync("api/lesson", createLessonDto);
 
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-        
-        var content = await response.Content.ReadFromJsonAsync<LessonDto>();
-        
-        Assert.Equal(createLessonDto.Name, content?.Name);
+
+        var createdLesson = await response.Content.ReadFromJsonAsync<LessonDto>();
+
+        Assert.NotNull(createdLesson);
+
+        Assert.Equal(createLessonDto.Name, createdLesson.Name);
+
+        response = await _client.GetAsync("api/lesson");
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+        var lessonsFromDatabase = await response.Content.ReadFromJsonAsync<IEnumerable<LessonDto>>();
+
+        Assert.NotNull(lessonsFromDatabase);
+
+        Assert.Single(lessonsFromDatabase, x =>
+            x.Id == createdLesson.Id && x.Name == createdLesson.Name);
+    }
+
+    [Fact]
+    public async Task Put_WhenIdIsInvalid_ShouldReturnNotFound()
+    {
+        var updateLessonDto = new UpdateLessonDto
+        {
+            Name = "updated_lesson"
+        };
+
+        const string id = "invalid_id";
+
+        var response = await _client.PutAsJsonAsync($"api/lesson/{id}", updateLessonDto);
+
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task Put_WhenDtoIsInvalid_ShouldReturnBadRequest()
+    {
+        var updateLessonDto = new UpdateLessonDto
+        {
+            Name = new string('a', 50)
+        };
+
+        const string id = "some_id";
+
+        var response = await _client.PutAsJsonAsync($"api/lesson/{id}", updateLessonDto);
+
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task Put_WhenEverythingIsValid_ShouldUpdateLesson()
+    {
+        var createLessonDto = new CreateLessonDto
+        {
+            Name = "created_lesson"
+        };
+
+        var response = await _client.PostAsJsonAsync("api/lesson", createLessonDto);
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+        var createdLesson = await response.Content.ReadFromJsonAsync<LessonDto>();
+
+        Assert.NotNull(createdLesson);
+
+        Assert.Equal(createLessonDto.Name, createdLesson.Name);
+
+        var lessonId = createdLesson.Id;
+
+        var updateLessonDto = new UpdateLessonDto
+        {
+            Name = "updated_lesson"
+        };
+
+        response = await _client.PutAsJsonAsync($"api/lesson/{lessonId}", updateLessonDto);
+
+        Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+
+        var updatedLessonsCollection = await _client.GetFromJsonAsync<IEnumerable<LessonDto>>($"api/lesson");
+
+        Assert.NotNull(updatedLessonsCollection);
+
+        Assert.Contains(updatedLessonsCollection, x =>
+            x.Id == lessonId && x.Name == updateLessonDto.Name);
+    }
+
+    [Fact]
+    public async Task Delete_WhenIdIsInvalid_ShouldReturnNotFound()
+    {
+        const string id = "invalid_id";
+
+        var response = await _client.DeleteAsync($"api/lesson/{id}");
+
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task Delete_WhenIdIsValid_ShouldDeleteLesson()
+    {
+        var createLessonDto = new CreateLessonDto
+        {
+            Name = "created_lesson"
+        };
+
+        var response = await _client.PostAsJsonAsync("api/lesson", createLessonDto);
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+        var createdLesson = await response.Content.ReadFromJsonAsync<LessonDto>();
+
+        Assert.NotNull(createdLesson);
+
+        response = await _client.DeleteAsync($"api/lesson/{createdLesson.Id}");
+
+        Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+
+        var updatedLessonsCollection = await _client.GetFromJsonAsync<IEnumerable<LessonDto>>("api/lesson");
+
+        Assert.NotNull(updatedLessonsCollection);
+
+        Assert.DoesNotContain(updatedLessonsCollection, x =>
+            x.Id == createdLesson.Id);
     }
 }
