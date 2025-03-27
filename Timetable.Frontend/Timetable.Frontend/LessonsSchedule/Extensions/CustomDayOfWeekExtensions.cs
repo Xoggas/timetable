315134ca440be6c378d01626ddab00850d45233e
Xoggas <sashakor16@gmail.com>
2025-03-27 19:17:13 +0300

    Renamed DayOfWeekExtensions to CustomDayOfWeekExtensions

---
+++ Timetable.Frontend/Timetable.Frontend/LessonsSchedule/Extensions/CustomDayOfWeekExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Timetable.Frontend.LessonsSchedule.Models;
+
+namespace Timetable.Frontend.LessonsSchedule.Extensions;
+
+public static class CustomDayOfWeekExtensions
+{
+    public static string ToShortString(this CustomDayOfWeek dayOfWeek)
+    {
+        return dayOfWeek switch
+        {
+            CustomDayOfWeek.Monday => "ПН",
+            CustomDayOfWeek.Tuesday => "ВТ",
+            CustomDayOfWeek.Wednesday => "СР",
+            CustomDayOfWeek.Thursday => "ЧТ",
+            CustomDayOfWeek.Friday => "ПТ",
+            CustomDayOfWeek.Saturday => "СБ",
+            CustomDayOfWeek.Sunday => "ВС",
+            _ => throw new ArgumentOutOfRangeException(nameof(dayOfWeek), dayOfWeek, null)
+        };
+    }
+}
