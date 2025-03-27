c48c3983c9b2adab7059f56aa30aa21341598a96
Xoggas <sashakor16@gmail.com>
2025-03-27 19:16:00 +0300

    Added LessonListService to provide mock data

---
+++ Timetable.Frontend/Timetable.Frontend/LessonsSchedule/Services/LessonListService.cs
@@ -0,0 +1,15 @@
+﻿using Timetable.Frontend.LessonsSchedule.Models;
+
+namespace Timetable.Frontend.LessonsSchedule.Services;
+
+public sealed class LessonListService
+{
+    public IEnumerable<Lesson> GetAllLessons()
+    {
+        return Enumerable.Range(0, 30).Select(x => x + 1).Select(i => new Lesson
+        {
+            Id = $"{i}",
+            Name = $"Урок {i:D2}"
+        });
+    }
+}
