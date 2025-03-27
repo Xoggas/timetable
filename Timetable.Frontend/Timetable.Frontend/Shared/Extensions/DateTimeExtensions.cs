b7feef06e3eb4370ccd721203ca52cb60a9a01f0
Xoggas <sashakor16@gmail.com>
2025-03-27 19:14:42 +0300

    Updated DateTimeExtensions to work with CustomDayOfWeek

--- Timetable.Frontend/Timetable.Frontend/Shared/Extensions/DateTimeExtensions.cs
+++ Timetable.Frontend/Timetable.Frontend/Shared/Extensions/DateTimeExtensions.cs
@@ -1,20 +1,20 @@
-﻿using DayOfWeek = Timetable.Frontend.LessonsSchedule.Models.DayOfWeek;
+﻿using Timetable.Frontend.LessonsSchedule.Models;
 
 namespace Timetable.Frontend.Shared.Extensions;
 
 public static class DateTimeExtensions
 {
-    public static DayOfWeek GetNativeDayOfWeek(this DateTime dateTime)
+    public static CustomDayOfWeek GetNativeDayOfWeek(this DateTime dateTime)
     {
         return dateTime.DayOfWeek switch
         {
-            System.DayOfWeek.Monday => DayOfWeek.Monday,
-            System.DayOfWeek.Tuesday => DayOfWeek.Tuesday,
-            System.DayOfWeek.Wednesday => DayOfWeek.Wednesday,
-            System.DayOfWeek.Thursday => DayOfWeek.Thursday,
-            System.DayOfWeek.Friday => DayOfWeek.Friday,
-            System.DayOfWeek.Saturday => DayOfWeek.Saturday,
-            System.DayOfWeek.Sunday => DayOfWeek.Sunday,
+            DayOfWeek.Monday => CustomDayOfWeek.Monday,
+            DayOfWeek.Tuesday => CustomDayOfWeek.Tuesday,
+            DayOfWeek.Wednesday => CustomDayOfWeek.Wednesday,
+            DayOfWeek.Thursday => CustomDayOfWeek.Thursday,
+            DayOfWeek.Friday => CustomDayOfWeek.Friday,
+            DayOfWeek.Saturday => CustomDayOfWeek.Saturday,
+            DayOfWeek.Sunday => CustomDayOfWeek.Sunday,
             _ => throw new ArgumentOutOfRangeException()
         };
     }
