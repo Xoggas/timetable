826621f607c21152abb68b04d4ba6254abfe8723
Xoggas <sashakor16@gmail.com>
2025-03-29 16:27:51 +0300

    Made Lesson model name modifiable

--- Timetable.Frontend/Timetable.Frontend/LessonsSchedule/Models/Lesson.cs
+++ Timetable.Frontend/Timetable.Frontend/LessonsSchedule/Models/Lesson.cs
@@ -3,5 +3,5 @@
 public sealed class Lesson
 {
     public string Id { get; init; } = string.Empty;
-    public string Name { get; init; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
 }
