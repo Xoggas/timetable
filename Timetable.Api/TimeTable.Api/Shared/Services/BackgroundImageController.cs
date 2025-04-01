5a5edbaf2e58d2b604d2054bc2fc67b3afb4dd72
Xoggas <sashakor16@gmail.com>
2025-04-01 16:31:39 +0300

    Updated swagger docs of BackgroundImageController

--- Timetable.Api/TimeTable.Api/Shared/Services/BackgroundImageController.cs
+++ Timetable.Api/TimeTable.Api/Shared/Services/BackgroundImageController.cs
@@ -17,10 +17,17 @@ public sealed class BackgroundImageController : ControllerBase
         _backgroundImageProvider = backgroundImageProvider;
     }
 
+    /// <summary>
+    /// Retrieves a random background image depending on the current month or event.
+    /// </summary>
+    /// <returns>A random background image depending on the current month or event.</returns>
+    /// <response code="200">Successfully sent an image.</response>
     [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     public async Task<FileResult> Get()
     {
         var bytes = await _backgroundImageProvider.GetRandomBackgroundImageBytes();
+        
         return File(bytes, "image/jpeg");
     }
 }
