c3af0c80fb3ea3853c450bd21b354261bbd415dd
Xoggas <sashakor16@gmail.com>
2025-09-21 12:58:35 +0300

    Removed playlist components from frontend

--- Timetable.Frontend/BellsSchedule/Models/Playlist.cs
+++
@@ -1,9 +0,0 @@
-using Timetable.Frontend.LessonsSchedule.Models;
-
-namespace Timetable.Frontend.BellsSchedule.Models;
-
-public sealed class Playlist
-{
-    public CustomDayOfWeek DayOfWeek { get; init; }
-    public List<string> SoundFilesIds { get; init; } = [];
-}
